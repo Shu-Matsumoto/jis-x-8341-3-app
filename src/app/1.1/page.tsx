a9a7796a2a82307387b8c41d6e3f30edb8ded000
Shu-Matsumoto <motomashu@gmail.com>
2025-02-03 00:43:00 +0900

    1.1.1実装

--- src/app/1.1/page.tsx
+++ src/app/1.1/page.tsx
@@ -2,6 +2,25 @@ export default function Home() {
     return (
       <div>
         <header>1.1</header>
+        <div className="m-4">
+          <div>
+            <div>アイテム：1.1.1</div>
+            <div className="ms-4">
+              <div>bad case</div>
+              <div>
+                画像
+                <img src="/example-logo.png" alt="" />
+              </div>
+            </div>
+            <div className="ms-4">
+              <div>good case</div>
+              <div>
+                画像
+                <img src="/example-logo.png" alt="会社のロゴ: Example Corp." />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
